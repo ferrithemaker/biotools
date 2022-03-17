9100c4c0681212ac6d4d27e72afbfeb24c84dd24
Ferran <ferri.fc@gmail.com>
2022-03-17 22:48:58 +0100

    genbank info

--- biotools-node-red/genbankprocessing.js
+++ biotools-node-red/genbankprocessing.js
@@ -5,9 +5,12 @@ module.exports = function(RED) {
         var rawOutput;
         var cdsNumber = -1;
         var cds;
+        var genBankInfo;
         var cdsArray = [];
         var output = "";
         node.on('input', function(msg) {
+			// get INFO
+			genBankInfo = msg.payload.substring(0,msg.payload.indexOf('FEATURE'));
 			if (config.seqtype.toLowerCase() == "origin") {
 				// ORIGIN reads
 				rawOutput = msg.payload.substring(msg.payload.indexOf('ORIGIN') + 6);
@@ -20,6 +23,7 @@ module.exports = function(RED) {
 				}
 			}
 			if (config.seqtype.toLowerCase() == "cds") {
+				// CDS translation
 				while ((cdsNumber = msg.payload.indexOf("/translation=",cdsNumber+1)) >= 0) cdsArray.push(cdsNumber);
 				cds = cdsArray[config.number-1] + 14;
 				while (msg.payload[cds] != '"') {
@@ -28,12 +32,11 @@ module.exports = function(RED) {
 				}
 			}
 			if (config.seqtype.toLowerCase() == "origin") {
-				msg.payload = { "sequence": output.toUpperCase() ,"protein": "" ,"information": ""}
+				msg.payload = { "sequence": output.toUpperCase() ,"protein": "" ,"information": genBankInfo}
 			}
 			if (config.seqtype.toLowerCase() == "cds") {
-				msg.payload = { "sequence": "", "protein": output.toUpperCase() ,"information": ""}
+				msg.payload = { "sequence": "", "protein": output.toUpperCase() ,"information": genBankInfo}
 			}
-			msg.payload = output;
             node.send(msg);
         });
     }
